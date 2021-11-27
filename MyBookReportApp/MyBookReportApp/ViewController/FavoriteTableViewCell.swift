68b3d1b3f21ce2295a10d0e6089fa80f08a4bf76
BAEKYUJEONG <qordbwjd96@gmail.com>
2021-11-28 02:29:41 +0900

    [style] Favorite Storyboard 수정

--- MyBookReportApp/MyBookReportApp/ViewController/FavoriteTableViewCell.swift
+++ MyBookReportApp/MyBookReportApp/ViewController/FavoriteTableViewCell.swift
@@ -11,13 +11,20 @@ class FavoriteTableViewCell: UITableViewCell {
     
     static let identifier = "FavoriteTableViewCell"
 
-    
     @IBOutlet weak var favoriteImageView: UIImageView!
     @IBOutlet weak var favoriteTitle: UILabel!
     @IBOutlet weak var favoriteAuthor: UILabel!
     @IBOutlet weak var favoritePublisher: UILabel!
     
     func configureCell(row: UserFavoriteBook) {
+        favoriteImageView.layer.borderWidth = 0
+        favoriteImageView.layer.borderColor = CGColor.init(red: 256, green: 256, blue: 256, alpha: 1)
+        
+        favoriteImageView.layer.masksToBounds = false
+        favoriteImageView.layer.shadowColor = UIColor.gray.cgColor
+        favoriteImageView.layer.shadowOffset = CGSize(width: 3, height: 3)
+        favoriteImageView.layer.shadowOpacity = 0.3
+        
         favoriteTitle.text = row.bookTitle
         favoriteAuthor.text = row.author
         favoritePublisher.text = row.publisher
